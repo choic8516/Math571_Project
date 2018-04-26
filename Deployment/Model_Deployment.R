4f254754ddc9b9fa0915dd2d16dbff404ee7d0d6
kyuuuuuuuuuu <sageras.li@gmail.com>
2018-04-26 10:21:48 -0500

    3rd day knn

--- Deployment/Model_Deployment.R
+++ Deployment/Model_Deployment.R
@@ -3,5 +3,5 @@ library(plumber)
 pr <- plumb("Deployment/plumber.R")
 pr$run(port=8000)
 
-#curl --data "very_neg=1&neg=1&neutral=1&pos=1&very_pos=1" "http://localhost:8000/predict"
+# curl --data "very_neg=1&neg=1&neutral=1&pos=1&very_pos=1" "http://localhost:8000/predict"
 
