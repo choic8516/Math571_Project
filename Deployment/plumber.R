f9bd7b8c04a31b92249394d339c167e80af54aa6
cchoi15 <cchoi15@hawk.iit.edu>
2018-04-25 21:29:03 -0500

    Minor change

--- Deployment/plumber.R
+++ Deployment/plumber.R
@@ -8,15 +8,7 @@ load("Deployment/KNN_Model.RData")
 predict_knn <- function(very_neg, neg, neutral, pos, very_pos) {
         data <- list(very_neg, neg, neutral, pos, very_pos)
         prediction <- knn(train = k_train_xVars, test = data, cl = k_train_label, k = opt_k)
-        return(numeric(prediction))
-}
-
-# return(list(default.probability=unbox(prediction[1, 2])))
-
-#* @get /mean
-normalMean <- function(samples=10){
-        data <- rnorm(samples)
-        mean(data)
+        return(paste0("Predicted Dow Jones Index Movement: ", ifelse(prediction == 1, "UP", "DOWN" )))
 }
 
 #paste0("Predicted Dow Jones Index Movement: ", ifelse(prediction == 1, "UP", "DOWN" ))
